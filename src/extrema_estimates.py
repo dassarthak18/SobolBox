dac7959dc1d0f1f9e7c16b58767c081ee2f3f893
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2024-12-05 16:43:57 +0530

    reverted to TNC for local optima guarantees; fixed joblib issues

--- src/extrema_estimates.py
+++ src/extrema_estimates.py
@@ -1,7 +1,7 @@
 import numpy as np
 from joblib import Parallel, delayed
 from scipy.stats import qmc
-from scipy.optimize import minimize, differential_evolution
+from scipy.optimize import minimize
 
 # We treat neural networks as a general MIMO black box
 def black_box(sess, input_array, input_name, label_name, input_shape):
@@ -25,11 +25,9 @@ def extremum_best_guess(sess, lower_bounds, upper_bounds, input_name, label_name
 	sample = sampler.random(d)
 	sample_scaled = qmc.scale(sample, lower_bounds, upper_bounds)
 	# compute the outputs
-	sample_output = []
-	sample_output = Parallel(prefer="threads")(delayed(black_box)(sess, datapoint, input_name, label_name, input_shape) for datapoint in sample_scaled)
-	#for datapoint in sample_scaled:
-	#	sample_output.append(black_box(sess, datapoint, input_name, label_name, input_shape))
-	# compute the extrema estimates
+	sample_output = Parallel(n_jobs=-1, prefer="threads", require="sharedmem", max_nbytes="1G")(
+		delayed(black_box)(sess, datapoint, input_name, label_name, input_shape) for datapoint in sample_scaled
+	)
 	minima = [min(x) for x in zip(*sample_output)]
 	maxima = [max(x) for x in zip(*sample_output)]
 	# compute inputs for those guesses
@@ -47,7 +45,7 @@ def extremum_best_guess(sess, lower_bounds, upper_bounds, input_name, label_name
 				break
 	return [minima_inputs, maxima_inputs, minima, maxima]
 
-# Objective function generator for L-BFGS-B
+# Objective function generator for TNC
 def create_objective_function(sess, input_shape, input_name, label_name, index, is_minima=True):
 	def objective(x):
 		arr = black_box(sess, x, input_name, label_name, input_shape)
@@ -74,31 +72,31 @@ def extremum_refinement(sess, input_bounds):
 		# refine the minima estimate
 		minima_inputs = extremum_guess[0]
 		minima = extremum_guess[2]
-		updated_minima_inputs = []
-		updated_minima = []
-		for index in range(len(minima_inputs)):
-			objective = create_objective_function(sess, input_shape, input_name, label_name, index)
-			result = minimize(objective, method = 'TNC', bounds=bounds, x0=list(minima_inputs[index]), options={'maxfun': 100, 'maxCGit': 10, 'eta': 0.2})
-			if result.fun < minima[index]:
-				updated_minima_inputs.append(list(result.x))
-				updated_minima.append(result.fun)
-			else:
-				updated_minima_inputs.append(minima_inputs[index])
-				updated_minima.append(minima[index])
+		results_minima = Parallel(n_jobs=-1, prefer="threads", require="sharedmem", max_nbytes="1G")(
+			delayed(minimize)(
+				create_objective_function(sess, input_shape, input_name, label_name, index),
+				method='TNC',
+				bounds=bounds,
+				x0=list(minima_inputs[index]),
+				options={'maxfun': 100, 'maxCGit': 10, 'eta': 0.2}
+			) for index in range(len(minima_inputs))
+		)
+		updated_minima_inputs = [list(result.x) for result in results_minima]
+		updated_minima = [result.fun for result in results_minima]
 		# refine the maxima estimate
 		maxima_inputs = extremum_guess[1]
 		maxima = extremum_guess[3]
-		updated_maxima_inputs = []
-		updated_maxima = []
-		for index in range(len(maxima_inputs)):
-			objective = create_objective_function(sess, input_shape, input_name, label_name, index, is_minima=False)
-			result = minimize(objective, method = 'TNC', bounds=bounds, x0=list(maxima_inputs[index]), options={'maxfun': 100, 'maxCGit': 10, 'eta': 0.2})
-			if result.fun > maxima[index]:
-				updated_maxima_inputs.append(list(result.x))
-				updated_maxima.append(result.fun)
-			else:
-				updated_maxima_inputs.append(maxima_inputs[index])
-				updated_maxima.append(maxima[index])
+		results_maxima = Parallel(n_jobs=-1, prefer="threads", require="sharedmem", max_nbytes="1G")(
+			delayed(minimize)(
+				create_objective_function(sess, input_shape, input_name, label_name, index, is_minima=False),
+				method='TNC',
+				bounds=bounds,
+				x0=list(maxima_inputs[index]),
+				options={'maxfun': 100, 'maxCGit': 10, 'eta': 0.2}
+			) for index in range(len(maxima_inputs))
+		)
+		updated_maxima_inputs = [list(result.x) for result in results_maxima]
+		updated_maxima = [-result.fun for result in results_maxima]
 		return [minima_inputs, maxima_inputs, minima, maxima]
 	except ValueError:
 		raise ValueError("Number of parameters too high, quitting gracefully.")
