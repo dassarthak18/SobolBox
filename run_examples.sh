842e80093967701ead96fe7070cea3343c56b70c
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2025-02-16 23:51:59 +0530

    Update run_examples.sh

--- run_examples.sh
+++ run_examples.sh
@@ -31,6 +31,17 @@ mkdir ../results
 ./prepare_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_5_9_batch_2000.onnx ../examples/acasxu/vnnlib/prop_10.vnnlib
 ./run_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_5_9_batch_2000.onnx ../examples/acasxu/vnnlib/prop_10.vnnlib ../results/result_acasxu_10.txt 600
 
+./prepare_instance.sh v1 traffic ../examples/traffic_signs_recognition/onnx/3_30_30_QConv_16_3_QConv_32_2_Dense_43_ep_30.onnx ../examples/traffic_signs_recognition/vnnlib/model_30_idx_11985_eps_1.00000.vnnlib
+./run_instance.sh v1 traffic ../examples/traffic_signs_recognition/onnx/3_30_30_QConv_16_3_QConv_32_2_Dense_43_ep_30.onnx ../examples/traffic_signs_recognition/vnnlib/model_30_idx_11985_eps_1.00000.vnnlib ../results/result_traffic_1.txt 600
+./prepare_instance.sh v1 traffic ../examples/traffic_signs_recognition/onnx/3_30_30_QConv_16_3_QConv_32_2_Dense_43_ep_30.onnx ../examples/traffic_signs_recognition/vnnlib/model_30_idx_11985_eps_10.00000.vnnlib
+./run_instance.sh v1 traffic ../examples/traffic_signs_recognition/onnx/3_30_30_QConv_16_3_QConv_32_2_Dense_43_ep_30.onnx ../examples/traffic_signs_recognition/vnnlib/model_30_idx_11985_eps_10.00000.vnnlib ../results/result_traffic_2.txt 600
+./prepare_instance.sh v1 traffic ../examples/traffic_signs_recognition/onnx/3_30_30_QConv_16_3_QConv_32_2_Dense_43_ep_30.onnx ../examples/traffic_signs_recognition/vnnlib/model_30_idx_11985_eps_15.00000.vnnlib
+./run_instance.sh v1 traffic ../examples/traffic_signs_recognition/onnx/3_30_30_QConv_16_3_QConv_32_2_Dense_43_ep_30.onnx ../examples/traffic_signs_recognition/vnnlib/model_30_idx_11985_eps_15.00000.vnnlib ../results/result_traffic_3.txt 600
+./prepare_instance.sh v1 traffic ../examples/traffic_signs_recognition/onnx/3_30_30_QConv_16_3_QConv_32_2_Dense_43_ep_30.onnx ../examples/traffic_signs_recognition/vnnlib/model_30_idx_11985_eps_3.00000.vnnlib
+./run_instance.sh v1 traffic ../examples/traffic_signs_recognition/onnx/3_30_30_QConv_16_3_QConv_32_2_Dense_43_ep_30.onnx ../examples/traffic_signs_recognition/vnnlib/model_30_idx_11985_eps_3.00000.vnnlib ../results/result_traffic_4.txt 600
+./prepare_instance.sh v1 traffic ../examples/traffic_signs_recognition/onnx/3_30_30_QConv_16_3_QConv_32_2_Dense_43_ep_30.onnx ../examples/traffic_signs_recognition/vnnlib/model_30_idx_11985_eps_5.00000.vnnlib
+./run_instance.sh v1 traffic ../examples/traffic_signs_recognition/onnx/3_30_30_QConv_16_3_QConv_32_2_Dense_43_ep_30.onnx ../examples/traffic_signs_recognition/vnnlib/model_30_idx_11985_eps_5.00000.vnnlib ../results/result_traffic_5.txt 600
+
 clear
 
 echo -n "prop_1 of acasxu run2a_1_1_batch_2000: "
@@ -63,6 +74,22 @@ cat ../results/result_acasxu_9.txt
 echo " "
 echo -n "prop_10 of acasxu run2a_5_9_batch_2000: "
 cat ../results/result_acasxu_10.txt
+echo " "
+
+echo -n "prop_1 of traffic: "
+cat ../results/result_traffic_1.txt
+echo " "
+echo -n "prop_2 of traffic: "
+cat ../results/result_traffic_2.txt
+echo " "
+echo -n "prop_3 of traffic: "
+cat ../results/result_traffic_3.txt
+echo " "
+echo -n "prop_4 of traffic: "
+cat ../results/result_traffic_4.txt
+echo " "
+echo -n "prop_5 of traffic: "
+cat ../results/result_traffic_5.txt
 echo " "
 
 rm -rf ../results
