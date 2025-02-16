be445a2a5712afa853584dd29668110637eb3bc0
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2025-02-16 22:01:55 +0530

    Update install_tool.sh

--- vnncomp_scripts/install_tool.sh
+++ vnncomp_scripts/install_tool.sh
@@ -6,17 +6,16 @@ if [ "$1" != v1 ]; then
 	exit 1
 fi
 
-echo "Installing INNVerS"
+echo "Installing BoxRL-NNV"
 
 #installing tool
 
 #Python 3.7 or higher
 apt-get update && apt-get install -y software-properties-common gcc && add-apt-repository -y ppa:deadsnakes/ppa
-apt-get update && apt-get install -y python3.7 python3-distutils python3-pip python3-apt libprotoc-dev protobuf-compiler
+apt-get update && apt-get install -y python3 python3-distutils python3-pip python3-apt libprotoc-dev protobuf-compiler
 
 #apt install -y psmisc && #for killall, used in prepare_instance.sh
 
-pip3 install numpy #Numpy
-pip3 install ortools #Google OR-Tools for MILP optimization
-pip3 install onnx==1.8.1 #ONNX for .onnx file processing
-pip3 install z3-solver #z3 Theorem Prover for .vnnlib file processing
+git clone https://github.com/dassarthak18/BoxRL-NNV.git
+cd BoxRL-NNV
+pip3 install -r requirements.txt
