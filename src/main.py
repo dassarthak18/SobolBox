aee07889c569f1e9e86926aa9256421cd0ddb2e7
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2025-02-16 21:33:18 +0530

    raised ValueError

--- src/main.py
+++ src/main.py
@@ -22,8 +22,7 @@ input_ub = []
 for a in assertions:
     sexpr = a.sexpr()
     if "or" in a.sexpr().lower():
-        print("Disjunction detected in property specification, quitting gracefully.")
-        sys.exit(0)
+        raise ValueError("Disjunction detected in property specification, quitting gracefully.")
     if a.decl().name() in ["<=", ">="]:
         var = a.arg(0)
         var_name = var.decl().name()
