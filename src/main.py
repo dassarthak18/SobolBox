14d46c951223aa8b7d0f6fe7625a2422deea1fa1
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2025-02-16 22:55:51 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -18,50 +18,55 @@ bounds = {}
 input_lb = []
 input_ub = []
 
-for a in assertions:
-    sexpr = a.sexpr()
-    var = a.arg(0)
-    var_name = var.decl().name()
-    if "X" not in var_name:
-        continue
-    if a.decl().name() in ["<=", ">="]:
-        op = a.decl().name()
-        value = a.arg(1).as_decimal(15)
-        if "or" in a.sexpr().lower():
-            print("Disjunction detected in property specification, quitting gracefully.")
-        if var_name not in bounds:
-            bounds[var_name] = {}
-        if op == "<=":
-            bounds[var_name]['ub'] = float(value)
-        else:
-            bounds[var_name]['lb'] = float(value)
-    elif a.decl().name() == "or":
-        raise ValueError("Disjunction detected in property specification, quitting gracefully.")
+try:
+	for a in assertions:
+	    sexpr = a.sexpr()
+	    var = a.arg(0)
+	    var_name = var.decl().name()
+	    if "X" not in var_name:
+	        continue
+	    if a.decl().name() in ["<=", ">="]:
+	        op = a.decl().name()
+	        value = a.arg(1).as_decimal(15)
+	        if "or" in a.sexpr().lower():
+	            raise TypeError("Disjunction detected in property specification, quitting gracefully.")
+	        if var_name not in bounds:
+	            bounds[var_name] = {}
+	        if op == "<=":
+	            bounds[var_name]['ub'] = float(value)
+	        else:
+	            bounds[var_name]['lb'] = float(value)
+	    elif a.decl().name() == "or":
+	        raise TypeError("Disjunction detected in property specification, quitting gracefully.")
+	
+	sorted_keys = sorted(bounds.keys(), key=lambda name: int(name.split('_')[-1]))
+	for var in sorted_keys:
+	    var_bounds = bounds[var]
+	    input_lb.append(var_bounds.get('lb'))
+	    input_ub.append(var_bounds.get('ub'))
+	
+	# We load the ONNX file and get the output bounds
+	sess = rt.InferenceSession(onnxFile)
+	bound = extremum_refinement(sess, [input_lb, input_ub])
+	output_lb = bound[2]
+	output_ub = bound[3]
+	
+	# We check the property and write the answer into the result file
+	n = len(output_lb)
+	for i in range(n):
+	    Y_i = Real("Y_" + str(i))
+	    solver.add(Y_i >= output_lb[i])
+	    solver.add(Y_i <= output_ub[i])
+	
+	file1 = open(resultFile, 'w')
+	if str(solver.check()) == "sat":
+		s = "violated"
+	else:
+		s = "holds"
+	file1.write(s)
+	file1.close()
 
-sorted_keys = sorted(bounds.keys(), key=lambda name: int(name.split('_')[-1]))
-for var in sorted_keys:
-    var_bounds = bounds[var]
-    input_lb.append(var_bounds.get('lb'))
-    input_ub.append(var_bounds.get('ub'))
-
-# We load the ONNX file and get the output bounds
-sess = rt.InferenceSession(onnxFile)
-bound = extremum_refinement(sess, [input_lb, input_ub])
-output_lb = bound[2]
-output_ub = bound[3]
-
-# We check the property and write the answer into the result file
-n = len(output_lb)
-for i in range(n):
-    Y_i = Real("Y_" + str(i))
-    solver.add(Y_i >= output_lb[i])
-    solver.add(Y_i <= output_ub[i])
-
-file1 = open(resultFile, 'w')
-if str(solver.check()) == "sat":
-	s = "violated"
-else:
-	s = "holds"
-file1.write(s)
-file1.close()
-
+except TypeError:
+	file1 = open(resultFile, 'w')
+	file1.write("unknown")
+	file1.close()
