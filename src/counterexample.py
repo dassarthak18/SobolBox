00e5735f35e3ad76f435085f22d1b1ee6bf681d7
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2025-02-17 17:50:24 +0530

    Update counterexample.py

--- src/counterexample.py
+++ src/counterexample.py
@@ -5,27 +5,31 @@ def spuriousCE_check(solver, sess):
   input_name = sess.get_inputs()[0].name
   label_name = sess.get_outputs()[0].name
   input_shape = [dim if isinstance(dim, int) else 1 for dim in sess.get_inputs()[0].shape]
-  
-  model = solver.model()
-  inputs = []
-  outputs = []
-  for i in model:
-    if "X" in str(i):
-      inputs.append(str(i))
+
+  while str(solver.check()) == "sat":
+    model = solver.model()
+    input_vars = []
+    output_vars = []
+    input_vals = []
+    output_vals = []
+    for i in model:
+      if "X" in str(i):
+        input_vars.append(str(i))
+      else:
+        output_vars.append(str(i))
+    input_vars = sorted(input_vars)
+    output_vars = sorted(output_vars)
+    for i in range(len(input_vars)):
+      x = Real(input_vars[i])
+      input_vals[i] = float(model.eval(x).as_decimal(20))
+    for j in range(len(output_vars)):
+      y = Real(output_vars[j])
+      output_vals[j] = float(model.eval(y).as_decimal(20))
+    output_vals_true = black_box(sess, input_vals, input_name, label_name, input_shape)
+    
+    if np.allclose(output_vals, output_vals_true, atol=1e-15):
+      return "violated " + f"\n{str(model)}"
     else:
-      outputs.append(str(i))
-  inputs = sorted(inputs)
-  outputs = sorted(outputs)
-  for i in range(len(inputs)):
-    x = Real(inputs[i])
-    inputs[i] = float(model.eval(x).as_decimal(20))
-  for j in range(len(outputs)):
-    y = Real(outputs[j])
-    outputs[j] = float(model.eval(y).as_decimal(20))
-  output = black_box(sess, inputs, input_name, label_name, input_shape)
-  
-  if np.allclose(outputs, output, atol=1e-15):
-    s = "violated " + f"\n{str(model)}"
-  else:
-    s = "unknown"
-  return s
+      variables = [Real(x) for x in input_vars+output_vars]
+      solver.add(Or([v != model[v] for v in variables]))
+  return "unknown"
