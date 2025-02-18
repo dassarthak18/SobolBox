cbf533105a23d47bee1bfe31f931b577cdc4201b
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2025-02-18 23:10:46 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import sys, copy
 import onnxruntime as rt
-from extrema_estimates import *
+from extrema_estimates import extremum_refinement
+from counterexample import enumerateCE
 from z3 import *
 
 # We open the VNNLIB file and get the input bounds
@@ -50,67 +51,24 @@ try:
 	# We load the ONNX file and get the output bounds
 	sess = rt.InferenceSession(onnxFile)
 	bound = extremum_refinement(sess, [input_lb, input_ub])
-	output_lb_input = bound[0]
-	output_ub_input = bound[1]
-	output_lb = bound[2]
-	output_ub = bound[3]
-	
-	vars = []
-	candidates = []
-	input_name = sess.get_inputs()[0].name
-	label_name = sess.get_outputs()[0].name
-	input_shape = [dim if isinstance(dim, int) else 1 for dim in sess.get_inputs()[0].shape]
-	for i in range(len(output_lb_input[0])):
-		vars.append(f"X_{i}")
-	for j in range(len(output_lb)):
-		vars.append(f"Y_{j}")
-	var_list = [Real(v) for v in vars]
-	for i in range(len(output_lb_input)):
-		arr_lb = []
-		arr_ub = []
-		bb_lb = black_box(sess, output_lb_input[i], input_name, label_name, input_shape)
-		bb_ub = black_box(sess, output_ub_input[i], input_name, label_name, input_shape)
-		for val in output_lb_input[i]:
-			arr_lb.append(val)
-		for val in output_ub_input[i]:
-			arr_ub.append(val)
-		for val in bb_lb:
-			arr_lb.append(val)
-		for val in bb_ub:
-			arr_ub.append(val)
-		candidates.append(arr_lb)
-		candidates.append(arr_ub)
-	
+	output_lb = bound[0]
+	output_ub = bound[1]
+
 	# Adding the maxima and minima points to the SAT constraints
 	for i in range(len(output_lb)):
 	    Y_i = Real("Y_" + str(i))
 	    solver.add(Y_i >= output_lb[i])
 	    solver.add(Y_i <= output_ub[i])
 
-	# Adding the maxima and minima input output pairs to the SAT constraints
-	candidate_constraints = []
-	for candidate in candidates:
-		candidate_constraints.append(And([v == val for v, val in zip(var_list, candidate)]))
-	solver_CE = copy.deepcopy(solver)
-	solver.add(Sum([If(c, 1, 0) for c in candidate_constraints]) <= 1) # Weaker condition: might be unsafe if true, but confirmed safe if false
-	solver_CE.add(Sum([If(c, 1, 0) for c in candidate_constraints]) == 1) # Stronger condition: unsafe if true, might still be unsafe if false
-
 	# We check the property and write the answer into the result file
 	file1 = open(resultFile, 'w')
-	if str(solver.check()) == "sat":
-		if str(solver_CE.check()) == "sat":
-			model = solver.model()
-			s = "violated\nCE: "
-			for i in range(len(var_list)):
-				val = float(model.eval(var_list[i]).as_decimal(20))
-				s += str(var_list[i]) + " = " + str(val) + "\n"
-		else:
-			s = "unknown" # our analysis is inconclusive
-	else:
+	if str(solver.check()) == "unsat":
 		s = "holds"
+	else:
+		s = enumerateCE(solver, sess)
 	file1.write(s)
 	file1.close()
-
+	
 except:
 	file1 = open(resultFile, 'w')
 	file1.write("unknown")
