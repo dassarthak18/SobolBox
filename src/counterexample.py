8694b51011debf6eb3eb0dfbd0f953d97b34c38e
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2025-02-19 01:54:32 +0530

    Update counterexample.py

--- src/counterexample.py
+++ src/counterexample.py
@@ -20,7 +20,7 @@ def validateCE(model, sess):
   return False
 
 def enumerateCE(solver, sess):
-  variables = sorted([str(d) for d in model.decls()])
+  variables = sorted(list({str(d) for d in solver.assertions() if isinstance(d, ArithRef)}))
   numCEs = 0
   while str(solver.check()) == "sat":
     model = solver.model()
