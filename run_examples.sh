3ba0bd12d7a9042d53728d777087f2f90d9503a6
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2025-02-23 23:44:51 +0530

    Update run_examples.sh

--- run_examples.sh
+++ run_examples.sh
@@ -11,37 +11,37 @@ mkdir ../results
 mkdir ../cache
 
 ./prepare_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_1_1_batch_2000.onnx ../examples/acasxu/vnnlib/prop_1.vnnlib
-./run_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_1_1_batch_2000.onnx ../examples/acasxu/vnnlib/prop_1.vnnlib ../results/result_acasxu_1.txt 600
+./run_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_1_1_batch_2000.onnx ../examples/acasxu/vnnlib/prop_1.vnnlib ../results/result_acasxu_1.txt 3600
 ./prepare_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_1_1_batch_2000.onnx ../examples/acasxu/vnnlib/prop_2.vnnlib
-./run_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_1_1_batch_2000.onnx ../examples/acasxu/vnnlib/prop_2.vnnlib ../results/result_acasxu_2.txt 600
+./run_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_1_1_batch_2000.onnx ../examples/acasxu/vnnlib/prop_2.vnnlib ../results/result_acasxu_2.txt 3600
 ./prepare_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_1_1_batch_2000.onnx ../examples/acasxu/vnnlib/prop_3.vnnlib
-./run_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_1_1_batch_2000.onnx ../examples/acasxu/vnnlib/prop_3.vnnlib ../results/result_acasxu_3.txt 600
+./run_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_1_1_batch_2000.onnx ../examples/acasxu/vnnlib/prop_3.vnnlib ../results/result_acasxu_3.txt 3600
 ./prepare_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_1_1_batch_2000.onnx ../examples/acasxu/vnnlib/prop_4.vnnlib
-./run_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_1_1_batch_2000.onnx ../examples/acasxu/vnnlib/prop_4.vnnlib ../results/result_acasxu_4.txt 600
+./run_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_1_1_batch_2000.onnx ../examples/acasxu/vnnlib/prop_4.vnnlib ../results/result_acasxu_4.txt 3600
 ./prepare_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_1_1_batch_2000.onnx ../examples/acasxu/vnnlib/prop_5.vnnlib
-./run_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_1_1_batch_2000.onnx ../examples/acasxu/vnnlib/prop_5.vnnlib ../results/result_acasxu_5.txt 600
+./run_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_1_1_batch_2000.onnx ../examples/acasxu/vnnlib/prop_5.vnnlib ../results/result_acasxu_5.txt 3600
 
 ./prepare_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_5_9_batch_2000.onnx ../examples/acasxu/vnnlib/prop_6.vnnlib
-./run_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_5_9_batch_2000.onnx ../examples/acasxu/vnnlib/prop_6.vnnlib ../results/result_acasxu_6.txt 600
+./run_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_5_9_batch_2000.onnx ../examples/acasxu/vnnlib/prop_6.vnnlib ../results/result_acasxu_6.txt 3600
 ./prepare_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_5_9_batch_2000.onnx ../examples/acasxu/vnnlib/prop_7.vnnlib
-./run_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_5_9_batch_2000.onnx ../examples/acasxu/vnnlib/prop_7.vnnlib ../results/result_acasxu_7.txt 600
+./run_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_5_9_batch_2000.onnx ../examples/acasxu/vnnlib/prop_7.vnnlib ../results/result_acasxu_7.txt 3600
 ./prepare_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_5_9_batch_2000.onnx ../examples/acasxu/vnnlib/prop_8.vnnlib
-./run_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_5_9_batch_2000.onnx ../examples/acasxu/vnnlib/prop_8.vnnlib ../results/result_acasxu_8.txt 600
+./run_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_5_9_batch_2000.onnx ../examples/acasxu/vnnlib/prop_8.vnnlib ../results/result_acasxu_8.txt 3600
 ./prepare_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_5_9_batch_2000.onnx ../examples/acasxu/vnnlib/prop_9.vnnlib
-./run_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_5_9_batch_2000.onnx ../examples/acasxu/vnnlib/prop_9.vnnlib ../results/result_acasxu_9.txt 600
+./run_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_5_9_batch_2000.onnx ../examples/acasxu/vnnlib/prop_9.vnnlib ../results/result_acasxu_9.txt 3600
 ./prepare_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_5_9_batch_2000.onnx ../examples/acasxu/vnnlib/prop_10.vnnlib
-./run_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_5_9_batch_2000.onnx ../examples/acasxu/vnnlib/prop_10.vnnlib ../results/result_acasxu_10.txt 600
+./run_instance.sh v1 acasxu ../examples/acasxu/onnx/ACASXU_run2a_5_9_batch_2000.onnx ../examples/acasxu/vnnlib/prop_10.vnnlib ../results/result_acasxu_10.txt 3600
 
 ./prepare_instance.sh v1 traffic ../examples/traffic_signs_recognition/onnx/3_30_30_QConv_16_3_QConv_32_2_Dense_43_ep_30.onnx ../examples/traffic_signs_recognition/vnnlib/model_30_idx_11985_eps_1.00000.vnnlib
-./run_instance.sh v1 traffic ../examples/traffic_signs_recognition/onnx/3_30_30_QConv_16_3_QConv_32_2_Dense_43_ep_30.onnx ../examples/traffic_signs_recognition/vnnlib/model_30_idx_11985_eps_1.00000.vnnlib ../results/result_traffic_1.txt 600
+./run_instance.sh v1 traffic ../examples/traffic_signs_recognition/onnx/3_30_30_QConv_16_3_QConv_32_2_Dense_43_ep_30.onnx ../examples/traffic_signs_recognition/vnnlib/model_30_idx_11985_eps_1.00000.vnnlib ../results/result_traffic_1.txt 3600
 ./prepare_instance.sh v1 traffic ../examples/traffic_signs_recognition/onnx/3_30_30_QConv_16_3_QConv_32_2_Dense_43_ep_30.onnx ../examples/traffic_signs_recognition/vnnlib/model_30_idx_11985_eps_10.00000.vnnlib
-./run_instance.sh v1 traffic ../examples/traffic_signs_recognition/onnx/3_30_30_QConv_16_3_QConv_32_2_Dense_43_ep_30.onnx ../examples/traffic_signs_recognition/vnnlib/model_30_idx_11985_eps_10.00000.vnnlib ../results/result_traffic_2.txt 600
+./run_instance.sh v1 traffic ../examples/traffic_signs_recognition/onnx/3_30_30_QConv_16_3_QConv_32_2_Dense_43_ep_30.onnx ../examples/traffic_signs_recognition/vnnlib/model_30_idx_11985_eps_10.00000.vnnlib ../results/result_traffic_2.txt 3600
 ./prepare_instance.sh v1 traffic ../examples/traffic_signs_recognition/onnx/3_30_30_QConv_16_3_QConv_32_2_Dense_43_ep_30.onnx ../examples/traffic_signs_recognition/vnnlib/model_30_idx_11985_eps_15.00000.vnnlib
-./run_instance.sh v1 traffic ../examples/traffic_signs_recognition/onnx/3_30_30_QConv_16_3_QConv_32_2_Dense_43_ep_30.onnx ../examples/traffic_signs_recognition/vnnlib/model_30_idx_11985_eps_15.00000.vnnlib ../results/result_traffic_3.txt 600
+./run_instance.sh v1 traffic ../examples/traffic_signs_recognition/onnx/3_30_30_QConv_16_3_QConv_32_2_Dense_43_ep_30.onnx ../examples/traffic_signs_recognition/vnnlib/model_30_idx_11985_eps_15.00000.vnnlib ../results/result_traffic_3.txt 3600
 ./prepare_instance.sh v1 traffic ../examples/traffic_signs_recognition/onnx/3_30_30_QConv_16_3_QConv_32_2_Dense_43_ep_30.onnx ../examples/traffic_signs_recognition/vnnlib/model_30_idx_11985_eps_3.00000.vnnlib
-./run_instance.sh v1 traffic ../examples/traffic_signs_recognition/onnx/3_30_30_QConv_16_3_QConv_32_2_Dense_43_ep_30.onnx ../examples/traffic_signs_recognition/vnnlib/model_30_idx_11985_eps_3.00000.vnnlib ../results/result_traffic_4.txt 600
+./run_instance.sh v1 traffic ../examples/traffic_signs_recognition/onnx/3_30_30_QConv_16_3_QConv_32_2_Dense_43_ep_30.onnx ../examples/traffic_signs_recognition/vnnlib/model_30_idx_11985_eps_3.00000.vnnlib ../results/result_traffic_4.txt 3600
 ./prepare_instance.sh v1 traffic ../examples/traffic_signs_recognition/onnx/3_30_30_QConv_16_3_QConv_32_2_Dense_43_ep_30.onnx ../examples/traffic_signs_recognition/vnnlib/model_30_idx_11985_eps_5.00000.vnnlib
-./run_instance.sh v1 traffic ../examples/traffic_signs_recognition/onnx/3_30_30_QConv_16_3_QConv_32_2_Dense_43_ep_30.onnx ../examples/traffic_signs_recognition/vnnlib/model_30_idx_11985_eps_5.00000.vnnlib ../results/result_traffic_5.txt 600
+./run_instance.sh v1 traffic ../examples/traffic_signs_recognition/onnx/3_30_30_QConv_16_3_QConv_32_2_Dense_43_ep_30.onnx ../examples/traffic_signs_recognition/vnnlib/model_30_idx_11985_eps_5.00000.vnnlib ../results/result_traffic_5.txt 3600
 
 clear
 
