1a6426b071223acba98bea1fb1cc5ecde02e48d1
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2025-02-24 03:51:21 +0530

    Update extrema_estimates.py

--- src/extrema_estimates.py
+++ src/extrema_estimates.py
@@ -20,7 +20,7 @@ def extremum_best_guess(sess, lower_bounds, upper_bounds, input_name, label_name
 	# check no. of parameters, gracefully quit if necessary
 	sampler = qmc.LatinHypercube(len(lower_bounds), scramble=False, optimization="lloyd", seed=np.random.default_rng())
 	inputsize = len(lower_bounds)
-	n_samples = 20*inputsize
+	n_samples = 15*inputsize
 	lower_bounds = np.array(lower_bounds)
 	upper_bounds = np.array(upper_bounds)
 	try:
