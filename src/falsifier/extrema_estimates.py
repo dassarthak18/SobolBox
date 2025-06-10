154bd8e32e6d51ecc994dc8056d0efa76e457e25
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2025-06-11 03:11:00 +0530

    Update extrema_estimates.py

--- src/falsifier/extrema_estimates.py
+++ src/falsifier/extrema_estimates.py
@@ -116,12 +116,12 @@ def extremum_refinement(sess, input_bounds, filename):
 		try:
 			with warnings.catch_warnings(record=True) as w:
 				warnings.simplefilter("always", UserWarning)
-				result = minimize(objective, method = 'trust-constr', bounds = bounds, x0 = x0, jac = '2-point', hess = SR1(), options = {'disp': False, 'gtol': 1e-6, 'maxiter': 300, 'xtol': 1e-14, 'barrier_tol':1e-12, 'initial_tr_radius': 1e-2})
+				result = minimize(objective, method = 'trust-constr', bounds = bounds, x0 = x0, jac = '2-point', hess = SR1(), options = {'disp': False, 'gtol': 1e-6, 'maxiter': 300, 'xtol': 1e-14})
 				for warning in w:
 					if "delta_grad == 0.0" in str(warning.message):
 						raise RuntimeError("Switch to zero Hessian")
 		except RuntimeError:
-			result = minimize(objective, method = 'trust-constr', bounds = bounds, x0 = x0, jac = '2-point', hess=lambda x: np.zeros((len(x), len(x))), options = {'disp': False, 'gtol': 1e-6, 'maxiter': 300, 'xtol': 1e-14, 'barrier_tol':1e-12, 'initial_tr_radius': 1e-2})
+			result = minimize(objective, method = 'trust-constr', bounds = bounds, x0 = x0, jac = '2-point', hess=lambda x: np.zeros((len(x), len(x))), options = {'disp': False, 'gtol': 1e-6, 'maxiter': 300, 'xtol': 1e-14})
 		xopt = result.x
 		updated_minima_inputs.append(list(xopt))
 		result = objective(xopt)
