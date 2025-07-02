8c8fc6669b5b8d371212b6a6e31630c69a84f695
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2025-07-02 22:43:32 +0530

    Update install_tool.sh

--- vnncomp_scripts/install_tool.sh
+++ vnncomp_scripts/install_tool.sh
@@ -14,7 +14,7 @@ echo "Installing SobolBox"
 apt-get update && apt-get install -y software-properties-common gcc && add-apt-repository -y ppa:deadsnakes/ppa
 apt-get update && apt-get install -y python3 python3-distutils python3-pip python3-apt libprotoc-dev protobuf-compiler
 
-apt install -y psmisc && #for killall, used in prepare_instance.sh
+apt install -y psmisc #for killall, used in prepare_instance.sh
 
 #git clone https://github.com/dassarthak18/SobolBox.git
 #cd SobolBox
