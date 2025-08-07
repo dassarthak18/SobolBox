813ae6af0964c56e77e904171012246330dc1b1c
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2025-08-07 20:59:55 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,23 +1,9 @@
-import sys, copy, csv, ast, json, re
+import sys, copy
 import onnxruntime as rt
-from pathlib import Path
 from parser import parse
 from falsifier.extrema_estimates import extremum_refinement
 from falsifier.counterexample import SAT_check
 from z3 import *
-
-maxInt = sys.maxsize
-while True:
-	try:
-		csv.field_size_limit(maxInt)
-		break
-	except OverflowError:
-		maxInt = int(maxInt/10)
-csv.field_size_limit(sys.maxsize)
-
-# We create a cache directory
-cache_dir = Path("..") / "cache"
-cache_dir.mkdir(parents=True, exist_ok=True)
 
 # We open the VNNLIB file and get the input bounds
 if sys.argv[1] == "--deep":
@@ -34,12 +20,8 @@ with open(propertyFile) as f:
 decls = {}
 assertions = parse_smt2_string(smt, decls=decls)
 solver = Solver()
-#solver_2 = Solver()
 for a in assertions:
   solver.add(a)
-  #names = {str(v) for v in a.children()}
-  #if any(re.match(r'^Y_\d+$', n) for n in names):
-  #  solver_2.add(a)
 solver_2 = copy.deepcopy(solver)
 try:
   print("Extracting input bounds.")
@@ -66,31 +48,11 @@ for j in bounds_dict:
     # We load the ONNX file and get the output bounds
     print("Extracting output bounds.")
     sess = rt.InferenceSession(onnxFile)
-    file_path = Path(onnxFile)
-    filename = file_path.name
-    boundsCacheFile = "../cache/" + filename[:-5] + "_bounds.csv"
-    cacheFound = False
-    if Path(boundsCacheFile).exists():
-      with open(boundsCacheFile, mode='r', newline='') as cacheFile:
-        reader = csv.reader(cacheFile, delimiter='|')
-        for row in reader:
-          fetched_input_lb = json.loads(row[0])
-          fetched_input_ub = json.loads(row[1])
-          if input_lb == fetched_input_lb and input_ub == fetched_input_ub:
-            output_lb = json.loads(row[2])
-            output_ub = json.loads(row[3])
-            output_lb_inputs = json.loads(row[4])
-            output_ub_inputs = json.loads(row[5])
-            cacheFound = True
-            print("Extracted output bounds from cache.")
-            break
-
-    if not cacheFound:
-      bound = extremum_refinement(sess, [input_lb, input_ub], filename)
-      output_lb = bound[0]
-      output_ub = bound[1]
-      output_lb_inputs = bound[2]
-      output_ub_inputs = bound[3]
+	bound = extremum_refinement(sess, [input_lb, input_ub])
+	output_lb = bound[0]
+	output_ub = bound[1]
+	output_lb_inputs = bound[2]
+	output_ub_inputs = bound[3]
 
     # Adding the maxima and minima points to the SAT constraints
     for i in range(len(output_lb)):
