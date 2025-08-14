598f9be1f8df5f850d39d3b8bf801466685c0b70
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2025-08-14 23:37:33 +0530

    Update memo_store.py

--- src/memo_store.py
+++ src/memo_store.py
@@ -1,16 +1,4 @@
-import gzip, pickle, os
-
-MEMO_FILE = "memo.pkl.gz"
-
-if os.path.exists(MEMO_FILE):
-  with gzip.open(MEMO_FILE, "rb") as f:
-    memo = pickle.load(f)
-else:
-  memo = {}
-
-def save_memo():
-  with gzip.open(MEMO_FILE, "wb") as f:
-    pickle.dump(memo, f)
+memo = {}
 
 def clear_memo():
   memo.clear()
