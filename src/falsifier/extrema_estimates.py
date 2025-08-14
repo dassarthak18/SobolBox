b7e438306be7d777f3e32ba51d86a44b7cba06b0
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2025-08-14 21:55:38 +0530

    Update extrema_estimates.py

--- src/falsifier/extrema_estimates.py
+++ src/falsifier/extrema_estimates.py
@@ -97,7 +97,7 @@ def extremum_refinement(sess, input_bounds):
             topk_points = np.vstack([topk_points, center_point])
         topk_maxs.append(topk_points)
         
-    results = Parallel(n_jobs=cpu_count(), backend="threading")(
+    results = Parallel(n_jobs=min(n_outputs,cpu_count()), backend="threading")(
         delayed(optimize_extrema)(
             sess, input_bounds, input_name, label_name, input_shape, i, objective_mins, objective_maxs, topk_mins, topk_maxs
         )
