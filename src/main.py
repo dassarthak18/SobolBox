e75597edbec2dedc932aca21caa067c572e0ed5e
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2025-08-15 18:22:49 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from memo_store import memo, clear_memo
 
-import sys, os, pickle
+import sys, os, pickle, hashlib
 import onnxruntime as rt
 from falsifier.extrema_estimates import extremum_refinement
 from falsifier.counterexample import CE_search
@@ -25,6 +25,15 @@ def main():
         bounds_dict = pickle.load(f)
 
     s = "unknown"  # default if no CE is found
+    sess = rt.InferenceSession(onnxFile)
+    os.makedirs(".output_bounds", exist_ok=True)
+    bounds_cache_file = f".output_bounds/{benchmark}.pkl"
+    # Try to load cached output bounds
+    if os.path.exists(bounds_cache_file):
+        with open(bounds_cache_file, "rb") as f:
+            output_bounds_cache = pickle.load(f)
+    else:
+        output_bounds_cache = {}
 
     for j in bounds_dict:
         print(f"Sub-problem {j}.")
@@ -37,8 +46,14 @@ def main():
             return
 
         print("Extracting output bounds.")
-        sess = rt.InferenceSession(onnxFile)
-        output_lb, output_ub, output_lb_inputs, output_ub_inputs = extremum_refinement(sess, [input_lb, input_ub])
+        hash_key = hashlib.md5(pickle.dumps((input_lb, input_ub))).hexdigest()
+        
+        if hash_key in output_bounds_cache:
+            output_lb, output_ub, output_lb_inputs, output_ub_inputs = output_bounds_cache[hash_key]
+            print("Output bounds extracted.")
+        else:
+            output_lb, output_ub, output_lb_inputs, output_ub_inputs = extremum_refinement(sess, [input_lb, input_ub])
+            output_bounds_cache[hash_key] = (output_lb, output_ub, output_lb_inputs, output_ub_inputs)
 
         with open(resultFile, 'w') as file1:
             s = CE_search(smt, sess, input_lb, input_ub,
@@ -46,8 +61,13 @@ def main():
                           output_lb_inputs, output_ub_inputs, setting)
             if s[:3] == "sat":
                 file1.write(s)
-                sys.exit(0)
+                with open(bounds_cache_file, "wb") as f:
+                    pickle.dump(output_bounds_cache, f)
+                return
 
+    with open(bounds_cache_file, "wb") as f:
+        pickle.dump(output_bounds_cache, f)
+        
     # Write final result
     with open(resultFile, 'w') as file1:
         file1.write(s)
